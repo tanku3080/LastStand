5980a496730c80bb1323b6bd547015f1baacdd0e
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2020-12-05 14:41:58 +0900

    playerのヌルぽを修正

--- Assets/scripts/NewScript/Player.cs
+++ Assets/scripts/NewScript/Player.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : PlayerBase
 {
-    [SerializeField] Animator animator;
     private void Start()
     {
         Rd = gameObject.GetComponent<Rigidbody>();
-        animator = Anime.GetComponent<Animator>();
+        Anime = gameObject.GetComponent<Animator>();
     }
     //人間は大雑把に八方向に移動できる。ロボットは不便な仕様にしたほうが良い？
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            animator.SetBool("WalkF", true);
+            Anime.SetBool("WalkF", true);
             this.transform.position = (Vector3.forward* playerSpeed).normalized;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            animator.SetBool("Back", true);
+            Anime.SetBool("Back", true);
             this.transform.position = (Vector3.forward * playerSpeed / 2).normalized;
         }
     }

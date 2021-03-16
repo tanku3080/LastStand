7db333b6cbc0b52efb6273bbaf79c30cd19bab3e
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2021-03-17 04:00:34 +0900

    シネマシーンテスト用に新しくシーンを作った。

---
+++ Assets/scripts/MovieSceneSet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StarStatus
+{
+    Move,EnemyPoint,Idol
+}
+public class MovieSceneSet : MonoBehaviour
+{
+    StarStatus Star;
+    bool collFlag = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Star)
+        {
+            case StarStatus.Move:
+                Moving();
+                break;
+            case StarStatus.EnemyPoint:
+                break;
+            case StarStatus.Idol:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Moving()
+    {
+        if (collFlag)
+        {
+            collFlag = false;
+        }
+    }
+}
